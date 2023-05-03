28e6c5b45a3160c90174987397db02e8b7767117
azemetre <azemetre@gmail.com>
2023-05-02 22:35:22 -0400

    feat: adding CloneFromReposJson func

--- pkg/piscator/piscator.go
+++ pkg/piscator/piscator.go
@@ -1,22 +1,29 @@
 package piscator
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
+	"os"
+	"os/exec"
 	"path"
+	"sync"
 )
 
-// make a post request to the GitHub API where you pass in an organization or user name
-
 type RepoModel struct {
-	Url      string
-	Name     string
-	Language string
-	Private  bool
-	Size     byte
+	Url     string `json:"html_url"`
+	Name    string `json:"name"`
+	Lang    string `json:"language"`
+	Private bool   `json:"private"`
+	Size    int    `json:"size"`
 }
 
-var RepoCollection []RepoModel
+type RepoCollection struct {
+	Repos []*RepoModel `json:"repos"`
+}
 
 func GetRepos(name string, isOrg, isPrivate bool) string {
 	var githubURL string
@@ -37,7 +44,91 @@ func GetRepos(name string, isOrg, isPrivate bool) string {
 	if isPrivate {
 		params.Add("private", "true")
 	}
-
 	githubURL = gh.String() + "?" + params.Encode()
-	return githubURL
+
+	res, err := http.Get(githubURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	var repos []RepoModel
+	err = json.NewDecoder(res.Body).Decode(&repos)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filteredRepos := []RepoModel{}
+	for _, repo := range repos {
+		if repo.Name != "" && repo.Url != "" {
+			filteredRepo := RepoModel{
+				Name:    repo.Name,
+				Lang:    repo.Lang,
+				Private: repo.Private,
+				Size:    repo.Size,
+				Url:     repo.Url,
+			}
+			filteredRepos = append(filteredRepos, filteredRepo)
+		}
+	}
+
+	// Marshal filteredRepos into JSON
+	jsonData, err := json.MarshalIndent(filteredRepos, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("repos.json", jsonData, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(jsonData)
+}
+
+type Repo struct {
+	Name string `json:"name"`
+	URL  string `json:"html_url"`
+}
+
+func CloneReposFromJson(jsonStr, name string) error {
+	// unmarshal the JSON string into a slice of Repo structs
+	var repos []Repo
+	if err := json.Unmarshal([]byte(jsonStr), &repos); err != nil {
+		return err
+	}
+
+	// create a directory for repos if it doesn't already exist
+	dir := name
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.Mkdir(dir, 0755); err != nil {
+			return err
+		}
+	}
+
+	// use a WaitGroup to wait for all clones to finish
+	var wg sync.WaitGroup
+	wg.Add(len(repos))
+
+	// clone each repo in a separate goroutine
+	for _, repo := range repos {
+		go func(repo Repo) {
+			defer wg.Done()
+
+			cmd := exec.Command("git", "clone", repo.URL)
+			cmd.Dir = dir
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Printf("failed to clone %s: %s\n", repo.URL, string(out))
+			}
+
+			// TODO: more succinct messaging
+			log.Printf("Cloned %s into %s/%s\n", repo.URL, dir, repo.Name)
+		}(repo)
+	}
+
+	// wait for all clones to finish
+	wg.Wait()
+
+	return nil
 }
