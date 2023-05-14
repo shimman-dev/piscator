41c26a97582b8e08ba31d8cf75e26b0c55b16449
azemetre <azemetre@gmail.com>
2023-05-14 10:12:34 -0400

    chore: renaming files to more fisherman names

---
+++ cmd/piscator/cast.go
@@ -0,0 +1,44 @@
+package piscator
+
+import (
+	"fmt"
+
+	"github.com/shimman-dev/piscator/pkg/piscator"
+	"github.com/spf13/cobra"
+)
+
+var isOrgBool, isPrivateBool, isForkedBool, makeFileBool bool
+
+var castCmd = &cobra.Command{
+	Use:     "cast",
+	Aliases: []string{"c"},
+	Short:   "generate a json struct of github repos",
+	Long:    `cast a net into the github sea capturing the URLs of repos from an user/org`,
+	Args:    cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Please provide a GitHub username")
+			return
+		}
+
+		name := args[0]
+		isOrgBool, _ = cmd.PersistentFlags().GetBool("org")
+		isPrivateBool, _ = cmd.PersistentFlags().GetBool("private")
+		isForkedBool, _ = cmd.PersistentFlags().GetBool("forked")
+		makeFileBool, _ = cmd.PersistentFlags().GetBool("makeFile")
+		res, err := piscator.GetRepos(name, isOrgBool, isPrivateBool, isForkedBool, makeFileBool)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(res)
+	},
+}
+
+func init() {
+	castCmd.PersistentFlags().BoolVarP(&isOrgBool, "org", "o", false, "Is an organization")
+	castCmd.PersistentFlags().BoolVarP(&isPrivateBool, "private", "p", false, "Include private repositories")
+	castCmd.PersistentFlags().BoolVarP(&isForkedBool, "forked", "x", false, "Include forked repositories")
+	castCmd.PersistentFlags().BoolVarP(&makeFileBool, "makeFile", "f", false, "Generate a repos.json file")
+
+	rootCmd.AddCommand(castCmd)
+}
---
+++ cmd/piscator/net.go
@@ -0,0 +1,47 @@
+package piscator
+
+import (
+	"fmt"
+
+	"github.com/shimman-dev/piscator/pkg/piscator"
+	"github.com/spf13/cobra"
+)
+
+var isVerbose bool
+
+var netCmd = &cobra.Command{
+	Use:     "net",
+	Aliases: []string{"c"},
+	Short:   "git clone collected repos",
+	Long:    `create a directory based on user/org name then create a git repo for each collection`,
+	Args:    cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Please provide a GitHub name")
+			return
+		}
+
+		name := args[0]
+		isOrgBool, _ = cmd.PersistentFlags().GetBool("org")
+		isPrivateBool, _ = cmd.PersistentFlags().GetBool("private")
+		isForkedBool, _ = cmd.PersistentFlags().GetBool("forked")
+		makeFileBool, _ = cmd.PersistentFlags().GetBool("makeFile")
+		res, err := piscator.GetRepos(name, isOrgBool, isPrivateBool, isForkedBool, makeFileBool)
+		if err != nil {
+			fmt.Println("Errors: %s", err)
+		}
+		isVerbose, _ = cmd.PersistentFlags().GetBool("verbose")
+		piscator.CloneReposFromJson(res, name, 5, isVerbose)
+		fmt.Println("success friend :)")
+	},
+}
+
+func init() {
+	netCmd.PersistentFlags().BoolVarP(&isOrgBool, "org", "o", false, "Is an organization")
+	netCmd.PersistentFlags().BoolVarP(&isPrivateBool, "private", "p", false, "Include private repositories")
+	netCmd.PersistentFlags().BoolVarP(&isForkedBool, "forked", "x", false, "Include forked repositories")
+	netCmd.PersistentFlags().BoolVarP(&makeFileBool, "makeFile", "f", false, "Generate a repos.json file")
+	netCmd.PersistentFlags().BoolVarP(&isVerbose, "verbose", "v", false, "logs detailed messaging to stdout")
+
+	rootCmd.AddCommand(netCmd)
+}
