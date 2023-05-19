28ad95bcd9f4dcd00d451f0e109aa9c9dc48034d
azemetre <azemetre@gmail.com>
2023-05-18 23:16:03 -0400

    chore: formatting help text

--- cmd/piscator/root.go
+++ cmd/piscator/root.go
@@ -10,7 +10,18 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "piscator",
 	Short: "piscator is a CLT for cloning GitHub repos",
-	Long:  `Embark on a grand voyage across the GitHub seas! Set sail to create a magnificent directory, inspired by the name of a fearless sailor or a mighty pirate. With each collection you reel in, a new Git repository shall be forged, like a sturdy ship ready to conquer the code oceans. Prepare yourself to navigate through the user's or organization's treasures, uncovering hidden gems and secret code islands. Will you include private repositories, like mysterious hidden coves? Or perhaps venture into the realm of forked repositories, tracing the footsteps of fellow sailors? As you reel in the collections, a legendary repos.json file shall be forged, capturing the essence of your brave expedition. Choose the winds of verbosity to whisper tales of each step or keep silent like a true sailor. Raise the anchor, set your course, and let the adventure begin!`,
+	Long: `Embark on a grand voyage across the GitHub seas! Set sail to create
+a magnificent directory, inspired by the name of a fearless sailor or a
+mighty pirate. With each collection you reel in, a new Git repository shall
+be forged, like a sturdy ship ready to conquer the code oceans. Prepare
+yourself to navigate through the user's or organization's treasures,
+uncovering hidden gems and secret code islands. Will you include private
+repositories, like mysterious hidden coves? Or perhaps venture into the
+realm of forked repositories, tracing the footsteps of fellow sailors? As
+you reel in the collections, a legendary repos.json file shall be forged,
+capturing the essence of your brave expedition. Choose the winds of
+verbosity to whisper tales of each step or keep silent like a true sailor.
+Raise the anchor, set your course, and let the adventure begin!`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("To see available commands run help")
 	},
