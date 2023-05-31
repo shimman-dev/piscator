7833dabcf1e6ae60e95f3d207d5fe77161388879
azemetre <azemetre@gmail.com>
2023-05-31 18:26:05 -0400

    feat: ability to filter by lang
    bug: enterprise domain woes

--- cmd/piscator/cast.go
+++ cmd/piscator/cast.go
@@ -10,7 +10,40 @@ import (
 )
 
 var isSelfBool, isOrgBool, isForkedBool, makeFileBool bool
-var githubToken string
+var languageFilter, name, githubToken, username, password, enterprise string
+
+func castRun(cmd *cobra.Command, args []string) {
+	if len(args) < 1 {
+		fmt.Println("Please provide a GitHub username")
+		return
+	}
+
+	name := args[0]
+	tokenFileBool := viper.GetString("github_token") // get token from viper
+	isSelfBool, _ := cmd.PersistentFlags().GetBool("self")
+	isOrgBool, _ := cmd.PersistentFlags().GetBool("org")
+	isForkedBool, _ := cmd.PersistentFlags().GetBool("forked")
+	makeFileBool, _ := cmd.PersistentFlags().GetBool("makeFile")
+
+	sleeper := &piscator.RealSleeper{}
+
+	res, err := piscator.GetRepos(http.DefaultClient, sleeper, name, tokenFileBool, username, password, enterprise, isSelfBool, isOrgBool, isForkedBool, makeFileBool)
+
+	if err != nil {
+		fmt.Printf("Errors: %s", err)
+		return
+	}
+
+	if languageFilter != "" {
+		res, err = piscator.RepoByLanguage(res, languageFilter)
+		if err != nil {
+			fmt.Printf("Error filtering repositories by language: %s", err)
+			return
+		}
+	}
+
+	fmt.Println(res)
+}
 
 var castCmd = &cobra.Command{
 	Use:     "cast",
@@ -22,28 +55,7 @@ repositories belonging to a user or organization, gathering a bountiful
 collection of code treasures. Navigate with ease, discovering new horizons, and
 charting your course towards software mastery.`,
 	Args: cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) < 1 {
-			fmt.Println("Please provide a GitHub username")
-			return
-		}
-
-		name := args[0]
-		tokenFileBool := viper.GetString("github_token") // get token from viper
-		isSelfBool, _ = cmd.PersistentFlags().GetBool("self")
-		isOrgBool, _ = cmd.PersistentFlags().GetBool("org")
-		isForkedBool, _ = cmd.PersistentFlags().GetBool("forked")
-		makeFileBool, _ = cmd.PersistentFlags().GetBool("makeFile")
-
-		sleeper := &piscator.RealSleeper{}
-
-		res, err := piscator.GetRepos(http.DefaultClient, sleeper, name, tokenFileBool, isSelfBool, isOrgBool, isForkedBool, makeFileBool)
-		if err != nil {
-			fmt.Printf("Errors: %s", err)
-			return
-		}
-		fmt.Println(res)
-	},
+	Run:  castRun,
 }
 
 func init() {
@@ -53,10 +65,22 @@ func init() {
 	castCmd.PersistentFlags().BoolVarP(&isOrgBool, "org", "o", false, "Is an organization")
 	castCmd.PersistentFlags().BoolVarP(&isForkedBool, "forked", "x", false, "Include forked repositories")
 	castCmd.PersistentFlags().BoolVarP(&makeFileBool, "makeFile", "f", false, "Generate a repos.json file")
-	castCmd.PersistentFlags().StringVarP(&githubToken, "token", "t", "", "GitHub personal access token")
 
-	// bind the token flag to a viper key
+	castCmd.PersistentFlags().StringVarP(&languageFilter, "language", "l", "", "Filter repositories by language(s)")
+
+	castCmd.PersistentFlags().StringVarP(&githubToken, "token", "t", "", "GitHub personal access token")
+	castCmd.PersistentFlags().StringVarP(&username, "username", "u", "", "GitHub username")
+	castCmd.PersistentFlags().StringVarP(&password, "password", "p", "", "GitHub password")
+	castCmd.PersistentFlags().StringVarP(&enterprise, "enterprise", "e", "", "GitHub Enterprise URL")
+
+	// bind the token flag to a env keys
 	viper.BindPFlag("github_token", castCmd.PersistentFlags().Lookup("token"))
+	viper.BindPFlag("username", castCmd.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("password", castCmd.PersistentFlags().Lookup("password"))
+
+	viper.BindEnv("github_token", "GITHUB_TOKEN")
+	viper.BindEnv("username", "GITHUB_USERNAME")
+	viper.BindEnv("password", "GITHUB_PASSWORD")
 
 	rootCmd.AddCommand(castCmd)
 }
--- cmd/piscator/reel.go
+++ cmd/piscator/reel.go
@@ -11,6 +11,55 @@ import (
 
 var isVerbose bool
 
+func reelRun(cmd *cobra.Command, args []string) {
+	if isSelfBool {
+		// Use the GitHub username associated with the token
+		name = "" // Set name to empty string for self
+	} else if len(args) < 1 && githubToken == "" && viper.GetString("github_token") == "" {
+		fmt.Println("Please provide a GitHub username or specify a token")
+		return
+	} else if len(args) >= 1 {
+		name = args[0]
+	}
+
+	if name == "" {
+		fmt.Println("Please provide a GitHub username or org name")
+		return
+	}
+
+	tokenFileBool := viper.GetString("github_token") // get token from viper
+	isForkedBool, _ = cmd.PersistentFlags().GetBool("forked")
+	makeFileBool, _ = cmd.PersistentFlags().GetBool("makeFile")
+
+	sleeper := &piscator.RealSleeper{}
+
+	res, err := piscator.GetRepos(http.DefaultClient, sleeper, name, tokenFileBool, username, password, enterprise, isSelfBool, isOrgBool, isForkedBool, makeFileBool)
+	if err != nil {
+		fmt.Printf("Errors: %s", err)
+		return
+	}
+
+	if languageFilter != "" {
+		res, err = piscator.RepoByLanguage(res, languageFilter)
+		if err != nil {
+			fmt.Printf("Error filtering repositories by language: %s", err)
+			return
+		}
+	}
+
+	concurrentLimit := int8(10)
+	isVerbose, _ = cmd.PersistentFlags().GetBool("verbose")
+
+	err = piscator.CloneReposFromJson(piscator.RealCommandExecutor{}, res, name, concurrentLimit, isVerbose)
+
+	if err != nil {
+		fmt.Printf("Errors: %s", err)
+		return
+	}
+
+	fmt.Println("success friend :)")
+}
+
 var reelCmd = &cobra.Command{
 	Use:     "reel",
 	Aliases: []string{"c"},
@@ -24,34 +73,7 @@ repositories, transforming them into valuable assets for your coding endeavors.
 Unleash your fishing prowess, reel in those repositories, and embark on a coding
 voyage like no other.`,
 	Args: cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) < 1 {
-			fmt.Println("Please provide a GitHub name")
-			return
-		}
-
-		name := args[0]
-		tokenFileBool := viper.GetString("github_token") // get token from viper
-		isSelfBool, _ = cmd.PersistentFlags().GetBool("self")
-		isOrgBool, _ = cmd.PersistentFlags().GetBool("org")
-		isForkedBool, _ = cmd.PersistentFlags().GetBool("forked")
-		makeFileBool, _ = cmd.PersistentFlags().GetBool("makeFile")
-
-		sleeper := &piscator.RealSleeper{}
-
-		res, err := piscator.GetRepos(http.DefaultClient, sleeper, name, tokenFileBool, isSelfBool, isOrgBool, isForkedBool, makeFileBool)
-		if err != nil {
-			fmt.Printf("Errors: %s", err)
-			return
-		}
-
-		concurrentLimit := int8(10)
-		isVerbose, _ = cmd.PersistentFlags().GetBool("verbose")
-
-		piscator.CloneReposFromJson(piscator.RealCommandExecutor{}, res, name, concurrentLimit, isVerbose)
-
-		fmt.Println("success friend :)")
-	},
+	Run:  reelRun,
 }
 
 func init() {
@@ -62,10 +84,22 @@ func init() {
 	reelCmd.PersistentFlags().BoolVarP(&isForkedBool, "forked", "x", false, "Include forked repositories")
 	reelCmd.PersistentFlags().BoolVarP(&makeFileBool, "makeFile", "f", false, "Generate a repos.json file")
 	reelCmd.PersistentFlags().BoolVarP(&isVerbose, "verbose", "v", false, "logs detailed messaging to stdout")
-	reelCmd.PersistentFlags().StringVarP(&githubToken, "token", "t", "", "GitHub personal access token")
 
-	// bind the token flag to a viper key
-	viper.BindPFlag("github_token", reelCmd.PersistentFlags().Lookup("token"))
+	reelCmd.PersistentFlags().StringVarP(&languageFilter, "language", "l", "", "Filter repositories by language(s)")
+
+	reelCmd.PersistentFlags().StringVarP(&githubToken, "token", "t", "", "GitHub personal access token")
+	reelCmd.PersistentFlags().StringVarP(&username, "username", "u", "", "GitHub username")
+	reelCmd.PersistentFlags().StringVarP(&password, "password", "p", "", "GitHub password")
+	reelCmd.PersistentFlags().StringVarP(&enterprise, "enterprise", "e", "", "GitHub Enterprise URL")
+
+	// bind the token flag to a env keys
+	viper.BindPFlag("github_token", castCmd.PersistentFlags().Lookup("token"))
+	viper.BindPFlag("username", castCmd.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("password", castCmd.PersistentFlags().Lookup("password"))
+
+	viper.BindEnv("github_token", "GITHUB_TOKEN")
+	viper.BindEnv("username", "GITHUB_USERNAME")
+	viper.BindEnv("password", "GITHUB_PASSWORD")
 
 	rootCmd.AddCommand(reelCmd)
 }
